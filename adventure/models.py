96410ed35a9e5c054259bc853ab25280073319aa
Victor Ruiz <ruizdvictor@gmail.com>
2020-03-09 23:11:49 -0400

    adds field to room object

--- adventure/models.py
+++ adventure/models.py
@@ -12,6 +12,7 @@ class Room(models.Model):
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
+    items = ArrayField(models.CharField(max_length=200, blank=True), default=list)
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
